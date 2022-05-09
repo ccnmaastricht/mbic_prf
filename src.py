463aa1f19906422f7034db7b8f37fb7bd02d609b
MSenden <mario.senden@gmail.com>
2022-05-09 12:04:00 +0200

    bug fixing and further reduced required code

--- src.py
+++ src.py
@@ -4,9 +4,9 @@ from scipy.fft import fft, ifft
 from scipy.signal import detrend, filtfilt, butter
 from cni_tlbx.gadgets import two_gamma, gaussian
 
-def simulate_data(self, ground_truth, stimulus, stimulus_duration, sampling_frequency):
-    good_voxels = sum(ground_truth['mask'])
-    total_voxels = len(ground_truth['mask'])
+def simulate_data(ground_truth, stimulus, stimulus_duration, sampling_frequency):
+    mask = ground_truth['masks']['all']
+    num_voxels = sum(mask)
     resolution, _, timepoints = stimulus.shape
     num_pixels = resolution**2
 
@@ -15,25 +15,22 @@ def simulate_data(self, ground_truth, stimulus, stimulus_duration, sampling_freq
                                   timepoints + len_hrf)
     hrf_fft = fft(two_gamma(time_vector))
 
-    r = np.linspace(-4.2, 4.2, resolution)
+    r = np.linspace(-8.4, 8.4, resolution)
     x_coordinates, y_coordinates = np.meshgrid(r,r)
     x_coordinates = x_coordinates.flatten()
     y_coordinates = -y_coordinates.flatten()
 
-    W = np.zeros((good_voxels, num_pixels))
-    mu_x = ground_truth['mu_x'][ground_truth['mask']]
-    mu_y = ground_truth['mu_y'][ground_truth['mask']]
-    sigma = ground_truth['sigma'][ground_truth['mask']]
-    for v in range(good_voxels):
+    W = np.zeros((num_voxels, num_pixels))
+    mu_x = ground_truth['mu_x'][mask]
+    mu_y = ground_truth['mu_y'][mask]
+    sigma = ground_truth['sigma'][mask]
+    for v in range(num_voxels):
         W[v, :] = gaussian(mu_x[v], mu_y[v], sigma[v], x_coordinates, y_coordinates)
 
     tc_fft = np.matmul(W, stimulus.reshape(num_pixels, timepoints)).transpose()
-    tc_fft = fft(np.append(tc_fft, np.zeros((len_hrf, good_voxels)), axis=0), axis=0)
+    tc_fft = fft(np.append(tc_fft, np.zeros((len_hrf, num_voxels)), axis=0), axis=0)
 
-    data = np.zeros((timepoints, total_voxels))
-    data[:, ground_truth['mask']] = np.real(ifft(tc_fft *
-                                                np.expand_dims(hrf_fft,
-                                                axis=1), axis=0))
+    data =  np.real(ifft(tc_fft * np.expand_dims(hrf_fft, axis=1), axis=0))
 
     variance = np.var(data, axis=0)
     peak = np.sqrt(2 * variance)
@@ -53,6 +50,6 @@ def simulate_data(self, ground_truth, stimulus, stimulus_duration, sampling_freq
     return data[:timepoints], W
 
 
-def preprocess(self, data, sampling_frequency):
+def preprocess(data, sampling_frequency):
     b, a = butter(2, 0.005,'hp', fs = sampling_frequency)
     return filtfilt(b, a, data, axis=0)
