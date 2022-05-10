e61373e150a9bf34ff42a052c28a7cb93f390300
MSenden <mario.senden@googlemail.com>
2022-05-10 12:25:08 +0200

    added stimulus generation

--- src.py
+++ src.py
@@ -1,5 +1,7 @@
 import numpy as np
 import nibabel as nib
+from PIL import Image
+from PIL import ImageDraw
 from scipy.fft import fft, ifft
 from scipy.signal import detrend, filtfilt, butter
 from cni_tlbx.gadgets import two_gamma, gaussian
@@ -53,3 +55,123 @@ def simulate_data(ground_truth, stimulus, stimulus_duration, sampling_frequency)
 def preprocess(data, sampling_frequency):
     b, a = butter(2, 0.005,'hp', fs = sampling_frequency)
     return filtfilt(b, a, data, axis=0)
+
+
+# simple code for creating wedge aperture (just for testing stuff. @Alex, I assume you will provide something more fancy)
+def create_wedge(direction, duty_cycle, temporal_frequency,
+                 stimulus_duration, time_steps, resolution=100):
+
+  if direction == 'clockwise':
+    sign = 1
+  elif direction == 'counterclockwise':
+    sign = -1
+
+  r = np.linspace(-1, 1, resolution)
+  X, Y = np.meshgrid(r, r)
+
+  angle = np.angle(X - Y * 1j)
+  radius = np.abs(X - Y * 1j)
+
+  stimulus = np.zeros((resolution, resolution, time_steps))
+  time_vec = np.linspace(0, stimulus_duration, time_steps)
+
+  phase_offset = np.angle(np.exp(2j * np.pi * time_vec * temporal_frequency))
+  phase_offset[phase_offset < 0] += 2 * np.pi
+
+  for t in range(time_steps):
+    new_angle = np.angle(np.exp((angle + sign* phase_offset[t]) * 1j))
+    stimulus[:, :, t] = (radius <= 1) & (np.abs(new_angle) <= (duty_cycle * np.pi))
+
+  return stimulus
+
+
+def create_ring(direction, duty_cycle, temporal_frequency,
+                stimulus_duration, time_steps, resolution=100):
+
+  r = np.linspace(-1, 1, resolution)
+  X, Y = np.meshgrid(r, r)
+
+  radius = np.abs(X - Y * 1j)
+
+  stimulus = np.zeros((resolution, resolution, time_steps))
+  time_vec = np.linspace(0, stimulus_duration, time_steps)
+
+  steps = (1 - duty_cycle) * time_vec * temporal_frequency
+
+  lower_radius = steps % (1 - duty_cycle)
+  upper_radius = lower_radius + duty_cycle
+
+  if direction == 'contracting':
+    lower_radius = lower_radius[::-1]
+    upper_radius = upper_radius[::-1]
+
+  for t in range(time_steps):
+    stimulus[:, :, t] = ((lower_radius[t] <= radius) &
+                         (radius <= upper_radius[t]))
+  return stimulus
+
+
+def create_bar(duty_cycle, time_steps, resolution=100):
+  def define_bar(x, y, width, height, angle):
+    img = Image.fromarray(np.zeros((resolution,
+                                    resolution)))
+    draw = ImageDraw.Draw(img)
+
+    rect = np.array([(-width / 2, -height / 2),
+                     (width / 2, -height / 2),
+                     (width / 2, height / 2),
+                     (-width / 2, height / 2),
+                     (-width / 2, -height / 2)])
+
+    theta = (np.pi / 180.0) * angle
+
+    R = np.array([[np.cos(theta), -np.sin(theta)],
+                  [np.sin(theta), np.cos(theta)]])
+
+    bar = np.dot(rect, R) + np.array([x, y])
+
+    draw.polygon([tuple(p) for p in bar], fill=1)
+
+    return np.asarray(img)
+
+  def rotate_coords(point, angle, origin=(50, 50)):
+    angle = np.radians(angle)
+
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + np.cos(angle) * (px - ox) - np.sin(angle) * (py - oy)
+    qy = oy + np.sin(angle) * (px - ox) + np.cos(angle) * (py - oy)
+
+    return qx, resolution - qy
+
+  r = np.linspace(-1, 1, resolution)
+  X, Y = np.meshgrid(r, r)
+
+  radius = np.abs(X - Y * 1j)
+
+  steps = int(duty_cycle**-1)
+  width = duty_cycle * 100
+
+  reps = int(time_steps / 8 / steps)
+
+  oris = [0, 45, 90, 135, 180, 225, 270, 315]
+
+  vals = np.linspace(width / 2, resolution - width / 2, steps, dtype=int)
+
+  coords = [(x, 50) for x in vals]
+
+  bars = []
+
+  for rep in range(reps):
+    for angle in oris:
+      new_coords = [rotate_coords(coord, angle)
+                    for coord in coords]
+
+      for x, y in new_coords:
+        bar = define_bar(x, y, width, 150, angle)
+        bars.append(bar * (radius <= 1))
+
+  stimulus = np.stack(bars, -1)
+
+  return stimulus
